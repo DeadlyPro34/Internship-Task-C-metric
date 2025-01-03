da0bee93e7316f6c2b6a6d3b73bcc46fa87e6e16
Akhil Biju Varghese <akhil.sbj@gmail.com>
2025-01-03 17:42:12 +0530

    ADD

--- Burger House Final Project/BH.js
+++ Burger House Final Project/BH.js
@@ -7,6 +7,24 @@ function hidemenu(){
     navLink.style.right = "-200px"
 }
 
+// Login
+const loginButton = document.getElementById('login-button'),
+        loginClose = document.getElementById('login-close'),
+        loginContent = document.getElementById('login-content')
+
+if (loginButton) {
+    loginButton.addEventListener('click', () => {
+        loginContent.classList.add('show-login');
+    });
+}
+
+if (loginClose) {
+    loginClose.addEventListener('click', () => {
+        loginContent.classList.remove('show-login');
+    });
+}
+
+
 // for smooth scroll
 window.addEventListener('scroll', reveal);
 
